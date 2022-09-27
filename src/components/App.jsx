38bf510829cbad9dd61fa65227e20e4651ed00f3
NikolayYevsovich <yevsovich.nick@gmail.com>
2022-09-27 18:19:39 +0300

    add local storage save and update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,26 @@ import { ContactList } from './ContactList/ContactList';
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const storageContacts = localStorage.getItem('contacts');
+    const parsedStorageContacts = JSON.parse(storageContacts);
+
+    if (parsedStorageContacts) {
+      this.setState({ contacts: parsedStorageContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   addContact = contact => {
     this.setState(prevState => {
